4144bf991464bab1f82cab111d4c46ac5dd0c976
SherlockLena <slavinskaya.lena2001@gmail.com>
2022-11-27 15:53:30 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    });
+
+    tabs.forEach(tab => {
+      tab.classList.remove('tabheader__item_active');
+    });
+  }
+
+  function showTabContent(number = 0) {
+    tabsContent[number].classList.add('show', 'fade');
+    tabsContent[number].classList.remove('hide');
+    tabs[number].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if(target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if(target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+});
