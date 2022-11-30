bf7dae2418cac13056efc54685190403d0d4f324
SherlockLena <slavinskaya.lena2001@gmail.com>
2022-11-30 16:44:31 +0200

    rest operator is added

--- js/script.js
+++ js/script.js
@@ -149,20 +149,21 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const modalTimerID = setTimeout(openModal, 3000);
+  const modalTimerID = setTimeout(openModal, 10000);
 
   window.addEventListener('scroll', showModalByScroll);
 
   //Menu items
 
   class MenuItem {
-    constructor(src, alt, name, description, price, parentSelector) {
+    constructor(src, alt, name, description, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.name = name;
       this.description = description;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
+      this.classes = classes;
       this.transfer = 37;
       this.exchangeToUAH();
     }
@@ -173,8 +174,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
     render() {
       const element = document.createElement('div');
+
+      if(!this.classes.includes('menu__item')) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      }
+
+      this.classes.forEach(className => element.classList.add(className));
+
       element.innerHTML = `
-      <div class="menu__item">
           <img src=${this.src} alt=${this.alt}>
           <h3 class="menu__item-subtitle">Меню "${this.name}"</h3>
           <div class="menu__item-descr">${this.description}</div>
@@ -183,7 +191,6 @@ window.addEventListener("DOMContentLoaded", () => {
               <div class="menu__item-cost">Цена:</div>
               <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
           </div>
-      </div>
       `;
       this.parent.append(element);
     }
